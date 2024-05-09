95d49b34b7f36d4136f589ffa8395d1440ee1e3f
one-chance <woody.front@gmail.com>
2024-05-09 15:24:28 +0900

    fix: Debug build error

--- src/components/lookbook/index.ts
+++ src/components/lookbook/index.ts
@@ -1,5 +1,4 @@
 export { default as Avatar } from './Avatar';
-export { default as CostumeList } from './CostumeList';
 export { default as CostumeSection } from './CostumeSection';
 export { default as EquipSection } from './EquipSection';
 export { default as SkinSection } from './SkinSection';
