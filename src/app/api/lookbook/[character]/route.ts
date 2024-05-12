43c483ef756303761cd5c08d1ba33335fb5a9701
one-chance <woody.front@gmail.com>
2024-05-12 20:18:24 +0900

    fix: Add user agent

--- src/app/api/lookbook/[character]/route.ts
+++ src/app/api/lookbook/[character]/route.ts
@@ -46,6 +46,9 @@ export async function GET(req: Request, { params }: { params: Params }) {
     headers: {
       method: 'GET',
       'Content-Type': 'image/png',
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+      // referer: 'https://baram.nexon.com',
     },
   });
 
