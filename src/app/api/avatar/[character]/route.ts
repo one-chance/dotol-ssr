8bb3e965bcd597c88cedd6c30b84d0fd696807b9
one-chance <woody.front@gmail.com>
2024-05-13 17:01:06 +0900

    fix: Add user agent

--- src/app/api/avatar/[character]/route.ts
+++ src/app/api/avatar/[character]/route.ts
@@ -46,7 +46,8 @@ export async function GET(req: Request, { params }: { params: Params }) {
     const res = await fetch(url, {
       headers: {
         'Content-Type': 'image/png',
-        referer: 'https://avatar.baram.nexon.com',
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
       },
     });
 
