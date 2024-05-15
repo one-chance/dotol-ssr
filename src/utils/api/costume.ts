79e75f5a6af3448f2b384fd57ad9d1a1783f28dd
one-chance <woody.front@gmail.com>
2024-05-15 15:37:40 +0900

    fix: Add alternative api

--- src/utils/api/costume.ts
+++ src/utils/api/costume.ts
@@ -1,4 +1,6 @@
+import { SKIN_LIST } from '@/contants';
 import { getAccessToken } from '../jwtUtils';
+import { Skin } from '@/types';
 
 export const getClothesList = async (keyword: string, part: string, page: number) => {
   const res = await fetch(
@@ -11,6 +13,27 @@ export const getClothesList = async (keyword: string, part: string, page: number
       },
     },
   );
+
   const data = await res.json();
   return data;
+};
+
+export const getAvatar = async (character: string, dir: string, skin: string, naked: string, items: string) => {
+  const skinParam = SKIN_LIST[skin as Skin] ?? `-1`;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER}/costumes/avatar/${character}?dir=${dir}&skin=${skinParam}&naked=${naked}&items=${items}`,
+    {
+      method: `GET`,
+      headers: {
+        'Content-Type': `image/png`,
+        Authorization: `Bearer ${getAccessToken()}`,
+        referer: `https://dotols.com`,
+      },
+    },
+  );
+
+  const blob = await res.blob();
+
+  return URL.createObjectURL(blob);
 };
