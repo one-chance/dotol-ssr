c4c8dfd6ce17de50480a2de63d984b25ce99d2af
one-chance <woody.front@gmail.com>
2024-05-16 17:50:03 +0900

    fix: Update lookbook api header options

--- src/app/api/lookbook/[character]/route.ts
+++ src/app/api/lookbook/[character]/route.ts
@@ -1,43 +1,43 @@
+import { NextRequest } from 'next/server';
 import { SKIN_LIST } from '@/contants';
 import { Skin } from '@/types';
 
-type Params = {
-  character: string;
-};
-
 const whiteList = ['http://localhost:5173', 'https://dotols.com'];
 
-export async function GET(req: Request, { params }: { params: Params }) {
-  const { character } = params;
-  const { searchParams } = new URL(req.url!);
-  const referer = req.headers.get('referer') || '';
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const character = decodeURI(request.nextUrl.pathname.replace('/api/avatar/', ''));
+  const [name, server] = character.split('@')!;
 
-  try {
-    if (!referer) {
-      throw new Error('Forbidden');
-    }
+  // const referer = req.headers.get('referer') || '';
 
-    const refererDomain = new URL(referer).origin;
+  // try {
+  //   if (!referer) {
+  //     throw new Error('Forbidden');
+  //   }
 
-    if (!whiteList.includes(refererDomain)) {
-      throw new Error('Forbidden');
-    }
-  } catch {
-    return new Response('Forbidden', { status: 403, statusText: 'Forbidden' });
+  //   const refererDomain = new URL(referer).origin;
+
+  //   if (!whiteList.includes(refererDomain)) {
+  //     throw new Error('Forbidden');
+  //   }
+  // } catch {
+  //   return new Response('Forbidden', { status: 403, statusText: 'Forbidden' });
+  // }
+
+  const urlParams = new URLSearchParams({ is: '1' });
+  if (searchParams.get('dir')) {
+    urlParams.set('changeDir', searchParams.get('dir')!);
   }
-
-  const [name, server] = character.split('@');
-  const skinParam = SKIN_LIST[searchParams.get('skin') as Skin] ?? '-1';
-  const equipParam = searchParams.get('items') ?? '';
-
-  const urlParams = new URLSearchParams();
-  urlParams.set('is', '1');
-  urlParams.set('changeDir', searchParams.get('dir') ?? '2');
-  urlParams.set('ed', searchParams.get('naked') ?? 'n');
-  urlParams.set('sc', skinParam);
-
-  if (equipParam !== '') {
-    urlParams.set('previewEquip', equipParam.replaceAll(',', '|'));
+  if (searchParams.get('naked')) {
+    urlParams.set('ed', searchParams.get('naked')!);
+  }
+  if (searchParams.get('skin')) {
+    const skinParam = SKIN_LIST[searchParams.get('skin') as Skin] ?? '-1';
+    urlParams.set('sc', skinParam);
+  }
+  if (searchParams.get('items')) {
+    urlParams.set('previewEquip', searchParams.get('items')!.replaceAll(',', '|'));
   }
 
   const url = `https://avatar.baram.nexon.com/Profile/RenderAvatar/${server}/${name}?${urlParams.toString()}`;
@@ -45,14 +45,12 @@ export async function GET(req: Request, { params }: { params: Params }) {
   const res = await fetch(url, {
     headers: {
       origin: 'https://baram.nexon.com',
-      referer: 'https://baram.nexon.com/',
-      'Access-Control-Allow-Headers':
-        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+      'Acccess-Control-Allow-Headers': 'Origin, Content-Type',
       'Access-Control-Allow-Origin': 'https://baram.nexon.com',
-      'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+      'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS, POST, PUT',
       'Content-Type': 'image/png',
-      Date: new Date().toUTCString(),
-      'Request-Context': 'appId=cid-v1:602befdf-c942-47ae-8f9e-a1749f6ee32f',
+      // Date: new Date().toUTCString(),
+      // 'Request-Context': 'appId=cid-v1:602befdf-c942-47ae-8f9e-a1749f6ee32f',
     },
   });
 
@@ -61,6 +59,8 @@ export async function GET(req: Request, { params }: { params: Params }) {
   return new Response(imageData, {
     status: 200,
     headers: {
+      'Access-Control-Allow-Origin': 'https://dotols.com',
+      'Access-Control-Allow-Methods': 'GET',
       'Content-Type': 'image/png',
     },
   });
