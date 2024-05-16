0c110bc56ed10d9c556e4846b0ae13652a7ed9a3
one-chance <woody.front@gmail.com>
2024-05-16 16:57:15 +0900

    fix: Update next api route

--- src/app/api/avatar/[character]/route.ts
+++ src/app/api/avatar/[character]/route.ts
@@ -1,43 +1,27 @@
+import { NextRequest } from 'next/server';
 import { SKIN_LIST } from '@/contants';
 import { Skin } from '@/types';
 
-type Params = {
-  character: string;
-};
-
 const whiteList = ['http://localhost:5173', 'https://dotols.com'];
 
-export async function GET(req: Request, { params }: { params: Params }) {
-  const { character } = params;
-  const { searchParams } = new URL(req.url!);
-  const referer = req.headers.get('referer') || '';
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+  const character = decodeURI(request.nextUrl.pathname.replace('/api/avatar/', ''));
+  const [name, server] = character.split('@')!;
 
-  // try {
-  //   if (!referer) {
-  //     throw new Error('Forbidden');
-  //   }
-
-  //   const refererDomain = new URL(referer).origin;
-
-  //   if (!whiteList.includes(refererDomain)) {
-  //     throw new Error('Forbidden');
-  //   }
-  // } catch {
-  //   return new Response('Forbidden', { status: 403, statusText: 'Forbidden' });
-  // }
-
-  const [name, server] = character.split('@');
-  const skinParam = SKIN_LIST[searchParams.get('skin') as Skin] ?? '-1';
-  const equipParam = searchParams.get('items') ?? '';
-
-  const urlParams = new URLSearchParams();
-  urlParams.set('is', '1');
-  urlParams.set('changeDir', searchParams.get('dir') ?? '2');
-  urlParams.set('ed', searchParams.get('naked') ?? 'n');
-  urlParams.set('sc', skinParam);
-
-  if (equipParam !== '') {
-    urlParams.set('previewEquip', equipParam.replaceAll(',', '|'));
+  const urlParams = new URLSearchParams({ is: '1' });
+  if (searchParams.get('dir')) {
+    urlParams.set('changeDir', searchParams.get('dir')!);
+  }
+  if (searchParams.get('naked')) {
+    urlParams.set('ed', searchParams.get('naked')!);
+  }
+  if (searchParams.get('skin')) {
+    const skinParam = SKIN_LIST[searchParams.get('skin') as Skin] ?? '-1';
+    urlParams.set('sc', skinParam);
+  }
+  if (searchParams.get('items')) {
+    urlParams.set('previewEquip', searchParams.get('items')!.replaceAll(',', '|'));
   }
 
   const url = `https://avatar.baram.nexon.com/Profile/RenderAvatar/${server}/${name}?${urlParams.toString()}`;
@@ -61,6 +45,7 @@ export async function GET(req: Request, { params }: { params: Params }) {
   return new Response(imageData, {
     status: 200,
     headers: {
+      'Access-Control-Allow-Origin': 'https://dotols.com',
       'Content-Type': 'image/png',
     },
   });
