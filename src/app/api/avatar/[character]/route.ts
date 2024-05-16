ddcbb590c22e14b6e07e10024b12607bb37be428
one-chance <woody.front@gmail.com>
2024-05-16 17:27:51 +0900

    fix: Test fetch header

--- src/app/api/avatar/[character]/route.ts
+++ src/app/api/avatar/[character]/route.ts
@@ -28,15 +28,9 @@ export async function GET(request: NextRequest) {
 
   const res = await fetch(url, {
     headers: {
-      origin: 'https://baram.nexon.com',
-      referer: 'https://baram.nexon.com/',
-      'Access-Control-Allow-Headers':
-        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
       'Access-Control-Allow-Origin': 'https://baram.nexon.com',
-      'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+      'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS, POST, PUT',
       'Content-Type': 'image/png',
-      Date: new Date().toUTCString(),
-      // 'Request-Context': 'appId=cid-v1:602befdf-c942-47ae-8f9e-a1749f6ee32f',
     },
   });
 
