77e338a99af5bdf0ec71d47abe6ad1f2db7026eb
one-chance <woody.front@gmail.com>
2025-01-11 12:26:40 +0900

    feat: Update january costumes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,22 +79,19 @@ export default function Home() {
 
       <div className="flex flex-row flex-wrap gap-5">
         <div className="flex flex-col flex-1 border rounded gap-2.5 p-4">
-          <span className="text-lg font-semibold">12월 신규 치장</span>
+          <span className="text-lg font-semibold">1월 신규 치장</span>
 
           <div className="flex flex-col gap-2">
-            <span className="font-medium">멋진의상재봉틀 - 천자봉황 의상</span>
+            <span className="font-medium">멋진의상재봉틀 - 해님달님 의상</span>
             <div className="flex flex-row flex-wrap justify-center items-end min-w-[304px]">
-              <Image unoptimized src="/1.gif" alt="costume1" width={300} height={220} />
+              <Image unoptimized src="/1.gif" alt="costume1" width={200} height={200} />
               <Image unoptimized src="/2.gif" alt="costume2" width={200} height={200} />
             </div>
 
-            <span className="font-medium"> 의상수선가위</span>
+            <span className="font-medium"> 의상수선가위 - 미니미도사/영술사</span>
             <div className="flex flex-row flex-wrap justify-center items-end min-w-[304px]">
-              <Image unoptimized src="/3.gif" alt="costume3" width={180} height={180} />
-              <Image unoptimized src="/4.gif" alt="costume4" width={180} height={180} />
-            </div>
-            <div className="flex flex-row flex-wrap justify-center items-end min-w-[304px]">
-              <Image unoptimized src="/5.gif" alt="costume5" width={140} height={140} />
+              <Image unoptimized src="/3.gif" alt="costume3" width={200} height={180} />
+              <Image unoptimized src="/4.gif" alt="costume4" width={200} height={180} />
             </div>
           </div>
         </div>
